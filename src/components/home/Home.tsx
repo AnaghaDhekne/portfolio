debca693b4fb9bb348e588e8440812801d5410cc
Anagha Dhekne <anaghagdhekne7@gmail.com>
2025-03-03 09:39:43 -0600

    Update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -76,14 +76,6 @@ export default function BackgroundBoxesDemo() {
                     ))}
                 </motion.div>
 
-                <a
-                    href="https://drive.google.com/file/d/1tOsh7nYdPxCbr8rOiSWuSUBMGlFr6iji/view?usp=drive_link"
-                    download="Anagha_Dhekne_Resume.pdf"
-                    className="relative px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors mt-4 sm:mt-6"
-                >
-                    Download Resume
-                </a>
-
                 <motion.div
                     className="absolute bottom-10 justify-center cursor-pointer"
                     animate={{
